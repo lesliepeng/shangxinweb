9b7ad8da637b772c369d08790d283843daa18475
lesliepeng <xiaomeng1207@gmail.com>
2017-04-19 22:09:52 +0800

    first commmit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+    $('nav a').on('click', function() {
+    
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+    
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
+    
+        return false;
+    
+    })
+    
+    
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 80) {
+            $('section').each(function(i) {
+                if ($(this).position().top <= windscroll + 50) {
+                    $('nav a.current').removeClass('current');
+                    $('nav a').eq(i).addClass('current');
+                }
+            });
+    
+        } else {
+    
+           
+            $('nav a.current').removeClass('current');
+            $('nav a:first').addClass('current');
+        }
+    
+    }).scroll();
+});
+
---
+++ js/main_ng.js
@@ -0,0 +1,25 @@
+var myapp = angular.module("app",['ngAnimate']);
+
+myapp.controller("Shangxin",function($scope,$interval,$timeout){
+
+    $scope.status = 0;
+    $scope.map_back_url = "images/mapdots1.png";
+    $scope.count = -1/4;
+
+    $scope.startAnimation = function(){
+        $scope.Timer = $interval(function () {
+            //取6的余数，表示一共有6个状态
+            $scope.status = Math.floor($scope.count) % 6 ;
+            console.log($scope.status);
+            //根据status控制背景图
+            $scope.map_back_url = "images/mapdots" + $scope.status.toString() + ".png";
+            //表示每个状态停留多久，分母表示停留秒数
+            $scope.count += 1/4;
+        }, 1000);
+
+    }
+
+    $scope.startAnimation();
+
+
+});
